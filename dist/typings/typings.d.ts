4cba2577ad86e999faf32d72bf4fb05bf98d79e7
Nicolas Pirotte <nicolas.pirotte@collibra.com>
2019-03-05 10:20:18 +0100

    Add typings

---
+++ dist/typings/typings.d.ts
@@ -0,0 +1,12 @@
+import { default } from 'react';
+import * as React from 'react';
+
+export declare const ButtonWithDefault: default.FC = ({ children }) => (
+  <button>{children}</button>
+);
+
+export declare const ButtonWithStar: React.FC = ({ children }) => (
+  <button>{children}</button>
+);
+
+export { }
