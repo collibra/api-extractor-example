ce523782b3d019eb36706eca57773fc8084f18ca
Nicolas Pirotte <nicolas.pirotte@collibra.com>
2019-03-05 10:09:55 +0100

    continue

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 var ButtonWithDefault_1 = require("./components/ButtonWithDefault");
 exports.ButtonWithDefault = ButtonWithDefault_1.ButtonWithDefault;
 var ButtonWithStar_1 = require("./components/ButtonWithStar");
 exports.ButtonWithStar = ButtonWithStar_1.ButtonWithStar;
+var react_1 = __importDefault(require("react"));
+console.log(react_1.default);
